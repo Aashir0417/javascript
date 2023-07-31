494a13c6b2cf32d08fb9dbc10c187ae689ad0a16
Aashir <aashir0417@gmail.com>
2023-07-31 11:18:17 -0400

    javascript variables

---
+++ var.js
@@ -0,0 +1,55 @@
+/*  
+
+Monday : Learn html and css
+Tuesday : play video games
+Wednesday : Work on your Bnr360 project
+Thursday : Learn Javascript
+Friday : Rest
+Saturday : play video games
+Sunday : rest
+
+
+
+
+
+*/
+
+
+
+
+
+let day='Thursday'
+
+
+switch(day){
+        case 'Monday':
+            console.log("Learn html and css")
+            break;
+    
+        case 'Tuesday':
+            console.log("Play video games")
+            break;
+    
+        case 'Wednesday':
+            console.log("Work on your Bnr360 project")   
+            break;
+
+        case 'Thursday':
+            console.log("Rest")
+            break;
+
+        case 'Friday':
+            console.log("Rest")
+            break;
+
+        case 'Saturday':
+            console.log("Memorize your tables")
+            break;
+
+
+        case 'Sunday':
+            console.log("Play video games")
+            break;
+    
+        
+}
